809bc5c097ff37e7a5b38916fc27e9e7b3291097
Hjalmarandersson <55764548+Hjalmarandersson@users.noreply.github.com>
2026-01-20 07:53:10 +0100

    Updating Nextjs 12->16 + Remove Linnea

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
       <div className="flex items-center justify-between">
         <img src="/logo.svg" alt="" />
         <p className="text-sm text-gray-400">
-          © 2022 Nearcloud, All rights reserved.
+          © 2026 Nearcloud, All rights reserved.
         </p>
       </div>
     </footer>
